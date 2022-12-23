8625cd253380b404fdc1227700fa4303db90916e
phartoonax <phartoonax@gmail.com>
2022-12-23 16:56:37 +0700

    main feature done -> todo relationship,  alarm and profile

--- lib/notes_detail.dart
+++ lib/notes_detail.dart
@@ -1,7 +1,9 @@
 import 'package:backendless_sdk/backendless_sdk.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_expandable_fab/flutter_expandable_fab.dart';
 import 'package:intl/intl.dart';
 import 'notes_alkitab.dart';
+import 'dart:math' as math;
 
 class NotesDetail extends StatefulWidget {
   const NotesDetail({Key? key}) : super(key: key);
@@ -13,10 +15,34 @@ class NotesDetail extends StatefulWidget {
 class _NotesDetailState extends State<NotesDetail> {
   @override
   Widget build(BuildContext context) {
+    final _key = GlobalKey<ExpandableFabState>();
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: const Icon(Icons.check),
+      floatingActionButtonLocation: ExpandableFab.location,
+      floatingActionButton: ExpandableFab(
+        type: ExpandableFabType.up,
+        key: _key,
+        child: FloatingActionButton(
+          onPressed: (() {
+            final state = _key.currentState;
+            if (state != null) {
+              debugPrint('isOpen:${state.isOpen}');
+              state.toggle();
+            }
+          }),
+          child: const Icon(Icons.save),
+        ),
+        children: [
+          FloatingActionButton.extended(
+            onPressed: (() {}),
+            icon: const Icon(Icons.close),
+            label: Text("No"),
+          ),
+          FloatingActionButton.extended(
+            onPressed: (() {}),
+            icon: const Icon(Icons.check),
+            label: Text("Yes"),
+          ),
+        ],
       ),
       appBar: AppBar(
         title: const Text("Glory Living"),
